9e6c9b2d5a65c109e687f7288f404304c3ac3648
Nxsh <nathanael.mutua.m@gmail.com>
2025-06-12 19:25:35 +0300

    add post request to create a task - add json middleware - modify response to have HTML content - add create function to the post

--- index.js
+++ index.js
@@ -4,25 +4,38 @@ import { PrismaClient } from "@prisma/client";
 
 const app = express();
 
+app.use(express.json());
+
 const prisma = new PrismaClient();
 
 app.get("/", (req, res) => {
-  res.send(`
-        <h2>A Simple Task API</h2>
-        <p>This is a basic API created for Creating Reading Updating and Deleting tasks by Nash</p>
-        <hr/>        
-    `); // This will be displayed the moment we run the link
+  res.send(`<body style="display: flex; flex-direction: column; justify-content: center; align-items: center; min-height: 100vh"><h1>This is a simple Task API</h1><p>This project handles HTTP requests with endpoints.</p><hr><p>By Nash</p></body>`); // This will be displayed the moment we run the link
 });
 
 app.get("/tasks", async (req, res) => {
   try {
     const task = await prisma.task.findMany();
-    res.status(200).json(task);
+    res.status(200).json(task).send(`<h1>Fetched All Tasks</h1>`);
   } catch (e) {
-    res.send(`<h1 style="text-align: center;">Something Went Wrong Bruv!</h1>`);
-    res.status(404).json("Something Went Wrong!");
+    res.status(404).json("Something Went Wrong!").send(`<h1 style="text-align: center;">Something Went Wrong Bruv!</h1>`);
   }
 });
+
+app.post("/tasks", async(req, res) => {
+    try{
+        const {title, description} = req.body;
+        const newTask = await prisma.task.create({
+          data: {
+            title,
+            description
+          }
+        })
+        res.status(201).json(newTask);
+    } catch (e) {
+        res.status(500).json({ message: "Something Went Wrong!"})
+        console.log("We have an error!")
+    }
+})
 
 const port = process.env.PORT || 7000;
 
