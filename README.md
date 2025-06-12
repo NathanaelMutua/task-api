b463a92c3af077b95e6076d011ab110bda575200
Nxsh <nathanael.mutua.m@gmail.com>
2025-06-12 15:26:26 +0300

    add prisma configuration - add database details to `.env` file - add response to be send to browser from the endpoint '/' - alter the port to be listened on to '7000' - add script to 'package.json' for the watch flag, to enable automatic updating of the server(only works on node.js versions 18 and above) - add README.md - add dummy data from mokeroo to 'tasks.js' - add 'task' model to 'schema.prisma'

---
+++ README.md
@@ -0,0 +1,15 @@
+# Task API
+
+## Description
+
+This is a basic, simple, "not at all complex" implementation of the following concepts:
+
+- Node JS and Express JS
+- PostgreSQL & Prisma
+- Postman and Mokeroo for other utilities
+
+---
+
+## Introduction
+
+I will be updating this section to describe the process
--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const app = express();
 
 app.get("/", (req, res) => {
     res.send(`
-        <h1>Simple TASK API</h1>
-        <p>This is a basic Api created for Creating Reading Updating and Deleting tasks</p>
+        <h2>A Simple Task API</h2>
+        <p>This is a basic API created for Creating Reading Updating and Deleting tasks</p>
         <hr/>        
-    `) // Thi swill be displayed the moment we run the link
+    `) // This will be displayed the moment we run the link
 })
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 7000;
 
 app.listen(port, () => {
-    console.log(`My app is listening on port ${port}`)
+    console.log(`My app is listening on port ${port}`);
 })
